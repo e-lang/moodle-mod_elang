16ca502a6f9e1ff8a46bdcd1dfe6b7a707b80412
unknown <jhay@GOLLUM06.univ-lr.fr>
2013-04-10 13:40:34 +0200

    Form creation update

--- src/mod_form.php
+++ src/mod_form.php
@@ -6,10 +6,10 @@
  * It uses the standard core Moodle formslib. For more info about them, please
  * visit: http://docs.moodle.org/en/Development:lib/formslib.php
  *
- * @package    mod
+ * @package	mod
  * @subpackage elang
  * @copyright  2013 University of La Rochelle, France
- * @license    http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html CeCILL-B license
+ * @license	http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html CeCILL-B license
  */
 
 defined('MOODLE_INTERNAL') || die();
@@ -21,43 +21,43 @@ require_once($CFG->dirroot.'/course/moodleform_mod.php');
  */
 class mod_elang_mod_form extends moodleform_mod {
 
-    /**
-     * Defines forms elements
-     */
-    public function definition() {
+	/**
+	 * Defines forms elements
+	 */
+	public function definition() {
 
-        $mform = $this->_form;
+		$mform = $this->_form;
 
-        //-------------------------------------------------------------------------------
-        // Adding the "general" fieldset, where all the common settings are showed
-        $mform->addElement('header', 'general', get_string('general', 'form'));
+		//-------------------------------------------------------------------------------
+		// Adding the "general" fieldset, where all the common settings are showed
+		$mform->addElement('header', 'general', get_string('general', 'form'));
 
-        // Adding the standard "name" field
-        $mform->addElement('text', 'name', get_string('elangname', 'elang'), array('size'=>'64'));
-        if (!empty($CFG->formatstringstriptags)) {
-            $mform->setType('name', PARAM_TEXT);
-        } else {
-            $mform->setType('name', PARAM_CLEAN);
-        }
-        $mform->addRule('name', null, 'required', null, 'client');
-        $mform->addRule('name', get_string('maximumchars', '', 255), 'maxlength', 255, 'client');
-        $mform->addHelpButton('name', 'elangname', 'elang');
+		// Adding the standard "name" field
+		$mform->addElement('text', 'name', get_string('elangname', 'elang'), array('size'=>'64'));
+		if (!empty($CFG->formatstringstriptags)) {
+			$mform->setType('name', PARAM_TEXT);
+		} else {
+			$mform->setType('name', PARAM_CLEAN);
+		}
+		$mform->addRule('name', null, 'required', null, 'client');
+		$mform->addRule('name', get_string('maximumchars', '', 255), 'maxlength', 255, 'client');
+		$mform->addHelpButton('name', 'elangname', 'elang');
 
-        // Adding the standard "intro" and "introformat" fields
-        $this->add_intro_editor();
+		// Adding the standard "intro" and "introformat" fields
+		$this->add_intro_editor();
 
-        //-------------------------------------------------------------------------------
-        // Adding the rest of elang settings, spreeading all them into this fieldset
-        // or adding more fieldsets ('header' elements) if needed for better logic
-        //$mform->addElement('static', 'label1', 'elangsetting1', 'Your elang fields go here. Replace me!');
-		$options = array(NOGROUPS       => get_string('element1'),
-                         SEPARATEGROUPS => get_string('element2'),
-                         VISIBLEGROUPS  => get_string('element3'));
-        $mform->addElement('select', 'monid', 'Theme', $options, NOGROUPS);
+		//-------------------------------------------------------------------------------
+		// Adding the rest of elang settings, spreeading all them into this fieldset
+		// or adding more fieldsets ('header' elements) if needed for better logic
+		//$mform->addElement('static', 'label1', 'elangsetting1', 'Your elang fields go here. Replace me!');
+		$options = array(	NOGROUPS	   => get_string('element1'),
+							SEPARATEGROUPS => get_string('element2'),
+							VISIBLEGROUPS  => get_string('element3'));
+		$mform->addElement('select', 'monid', 'Theme', $options, NOGROUPS);
 		
 		
-        $mform->addElement('header', 'elangfieldset', get_string('upload', 'elang'));
-        //$mform->addElement('static', 'label2', 'elangsetting2', 'Your elang fields go here. Replace me!');
+		$mform->addElement('header', 'elangfieldset', get_string('upload', 'elang'));
+		//$mform->addElement('static', 'label2', 'elangsetting2', 'Your elang fields go here. Replace me!');
 		$mform->addElement('filemanager', 'videos', get_string('videos', 'elang'), null, array('subdirs' => 0, 'maxbytes' => $maxbytes, 'maxfiles' => 20, 'accepted_types' => array('video')));
 		$mform->addHelpButton('videos', 'videos', 'elang');
 		$mform->addRule('videos', null, 'required', null, 'client');
@@ -73,11 +73,11 @@ class mod_elang_mod_form extends moodleform_mod {
 		$mform->addRule('poster', null, 'required', null, 'client');
 		//$mform->addHelpButton('name', 'poster_help', 'elang');
 		
-        //-------------------------------------------------------------------------------
-        // add standard elements, common to all modules
-        $this->standard_coursemodule_elements();
-        //-------------------------------------------------------------------------------
-        // add standard buttons, common to all modules
-        $this->add_action_buttons();
-    }
+		//-------------------------------------------------------------------------------
+		// add standard elements, common to all modules
+		$this->standard_coursemodule_elements();
+		//-------------------------------------------------------------------------------
+		// add standard buttons, common to all modules
+		$this->add_action_buttons();
+	}
 }
