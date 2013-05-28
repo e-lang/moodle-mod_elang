fa84765e2d80d076eff13d98f8385f396340c92d
mchevallier <mcheva03@GOLLUM15.univ-lr.fr>
2013-05-28 11:18:51 +0200

    Deleting Custom controls bar. Adding responsive video width

--- src/view/source/Video.js
+++ src/view/source/Video.js
@@ -1,12 +1,10 @@
 //
 var ajaxRequest = [[2.1,16.1],[16.1,20.1],[20.1,29.0],[30.1,38.306],[38.5,44.5],[49.5,55]];
 var soustitreajax = ["arduino-en.vtt","en_UK"];
-var videoajax = ["mov_bbb.ogv","mov_bbb.mp4"];
+var videoajax = ["mov_bbb.mp4","mov_bbb.mp4"];
 
-//taille de la video et de la slidebar
-var tailleVideo = 100%;
 //variable est à true si c'est la première fois que l'on joue la video
-var firstPlay = true;
+//var firstPlay = true;
 //variable à true si la vidéo est sur play, et sur false si la vidéo est en pause
 var boolPP = false;
 //slide bar défilant suivant l'évolution de la timeline de la vidéo
@@ -21,111 +19,15 @@ enyo.kind({
 	fit: true,
 	components:[
 		//Video
-		{kind: "elang.Video", name : "video", source:videoajax, sstitre : [soustitreajax], width: tailleVideo},
-		//slide bar défilant suivant l'évolution de la timeline de la vidéo
-		{kind: "onyx.Slider", name:"movingSlide", style: "display : none", lockBar: true, value: 0, onChange:"sliderVideoChanged"},
-		//Boutons de controle
-		{kind: "onyx.Toolbar", components: [
-			{kind: "onyx.Button", style: "height: 35px;width: 35px;", components: [{kind: "onyx.Icon", src: "im/pass_chap_prec.png", style:"background-position:center top; background-size: 22px 22px"}], ontap: "prev"},
-			{kind: "onyx.Button", style: "height: 35px;width: 35px;", name:"PP", components: [{kind: "onyx.Icon", name:"icone", src: "im/play.png", style:"background-position:center top;background-size: 22px 22px"}], ontap: "PP"},
-			{kind: "onyx.Button", style: "height: 35px;width: 35px;", components: [{kind: "onyx.Icon", src: "im/stop.png", style:"background-position:center top;background-size: 22px 22px"}], ontap: "stop"},
-			{kind: "onyx.Button", style: "height: 35px;width: 35px;", components: [{kind: "onyx.Icon", src: "im/pass_chap_suiv.png", style:"background-position:center top;background-size: 22px 22px"}], ontap: "next"},
-			{kind: "onyx.Button", style: "height: 35px;width: 35px;", components: [{kind: "onyx.Icon", src: "im/vol_down.png", style:"background-position:center top;background-size: 22px 22px"}], ontap: "volmoins"},
-			{kind: "onyx.Button", style: "height: 35px;width: 35px;", components: [{kind: "onyx.Icon", src: "im/vol_up.png", style:"background-position:center top;background-size: 22px 22px"}], ontap: "volplus"},
-			{kind: "onyx.Button", style: "height: 35px;width: 35px;", components: [{kind: "onyx.Icon", src: "im/vol_off_rouge.png", style:"background-position:center top;background-size: 20px 20px"}], ontap: "voloff"},
-			{kind: "onyx.Slider", name:"volum",style: "width: 40px;",min:0, max:1, lockBar: true, value: 1, onChange:"sliderChanged"}
-		]}
-	],
-	//Fonction gérant l'utilisation d'un bouton unique pour faire play et pause
-	PP: function(){
-		boolPP = !boolPP;
-		if(boolPP){
-			this.play();
-			this.$.icone.setSrc('im/pause.png');
-		}else{
-			this.pause();
-			this.$.icone.setSrc('im/play.png');
-		}
-	},
+		{kind: "elang.Video", name : "video", source:videoajax, sstitre : [soustitreajax]}//,
+		],
+	
 	//play de la video et gère le premier play de la video (initialisation de la slideBar)
-	play: function() {
-		if(firstPlay){
-			this.$.movingSlide.setMax(this.$.video.getMaxDuration());
-			this.$.movingSlide.setStyle("width: "+(tailleVideo-40)+"px");
-			firstPlay = !firstPlay;
-			movingSlide = this.$.movingSlide.getAttribute('id');
-			video = this.$.video.getId();
-			setInterval(function(){
-				document.getElementById(movingSlide).childNodes[0].style.width = 
-					((document.getElementById(video).currentTime/document.getElementById(video).duration)*100)+
-					"%";
-				document.getElementById(movingSlide).childNodes[2].style.left = 
-					((document.getElementById(video).currentTime/document.getElementById(video).duration)*100)+
-					"%";
-			},(this.$.video.getMaxDuration()/tailleVideo));
-		}
-		this.$.video.play();
-	},
-	//fonction pause de la video
-	pause: function() {
-		this.$.video.pause();
-	},
-	//fonction pause de la video + retour au début
-	stop: function() {
-		this.$.video.stop();
-		if(boolPP){
-			boolPP = false;
-			this.$.icone.setSrc('im/play.png');
-		}
-	},
-	//fonction renvoyant au début du chapitre puis au chapitre précédent
-	prev: function() {
-		if(!firstPlay){
-			this.$.video.prev();
-		}
-	},
-	//fonction renvoyant au chapitre suivant
-	next: function(){
-		if(!firstPlay){
-			this.$.video.next();
-		}
-	},
-	//augmentation du volume
-	volplus: function(){
-		this.$.video.volPlus();
-		this.majVol();
-	},
-	//diminution du volume
-	volmoins: function(){
-		this.$.video.volMoins();
-		this.majVol();
-	},
-	//mise à zéro du volume
-	voloff: function(){
-		this.$.video.volOff();
-		this.majVol();
-	},
-	//mise a jour de la barre son
-	majVol: function(){
-		document.getElementById(this.$.volum.getAttribute('id')).childNodes[0].style.width = 
-			(this.$.video.getVol()*100)+
-			"%";
-		document.getElementById(this.$.volum.getAttribute('id')).childNodes[2].style.left = 
-			(this.$.video.getVol()*100)+
-			"%";
-	},
-	//slider gérant le volume
-	sliderChanged: function(inSender, inEvent) {
-		this.$.video.setVolum(inSender.getValue());
-	},
-	//slider gérant le changement de la timeline de la vidéo en cliquant dessus
-	sliderVideoChanged: function(inSender, inEvent) {
-		this.$.video.setCurrentTime(inSender.getValue());
-	},
 	//mise en place de la balise video et de la balise de sous titres (track)
 	create: function(){
 		this.inherited(arguments);
 	}
+	
 });
 
 //balise video
@@ -133,20 +35,20 @@ enyo.kind({
 	name:"elang.Video",
 	published:{
 		source : [],
-		sstitre : [],
-		width : 200
+		sstitre : []
+		//width : 200
 	},
 	components:[
 		{
 			name: "html",
-			tag: "video",
+			tag: "video controls",
 			content: "Your user agent does not support the HTML5 Video element.",
 			components:[
 				{
 					name:"soustitre",
 					tag: "track",
 					attributes:{
-						kind:"captions",
+						kind:"captions"
 					}
 				}
 			]
@@ -198,32 +100,7 @@ enyo.kind({
 		
 	},
 	/***/
-	
-	/*fonction associées aux boutons de volumes*/
-	volPlus: function(){
-		if(document.getElementById(this.$.html.getAttribute('id')).volume < 0.9){
-			document.getElementById(this.$.html.getAttribute('id')).volume += 0.1;
-		}
-	},
-	volMoins: function(){
-		if(document.getElementById(this.$.html.getAttribute('id')).volume > 0.1){
-			document.getElementById(this.$.html.getAttribute('id')).volume -= 0.1;
-		}
-	},
-	volOff: function(){
-		document.getElementById(this.$.html.getAttribute('id')).volume = 0;
-	},
-	getVol: function(){
-		return document.getElementById(this.$.html.getAttribute('id')).volume;
-	},
-	/***/
-	
-	/*slider volum*/
-	setVolum: function(value) {
-		document.getElementById(this.$.html.getAttribute('id')).volume = value;
-	},
-	/***/
-	
+		
 	/*slider de durée*/
 	setCurrentTime: function(value) {
 		if(value < document.getElementById(this.$.html.getAttribute('id')).duration){
