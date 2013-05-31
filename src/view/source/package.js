7be978acdd3e6c4447ebbd87089adb0becf02f50
jjageneau <julie.jageneau@gmail.com>
2013-05-31 10:10:19 +0200

    add scrollbar

--- src/view/source/package.js
+++ src/view/source/package.js
@@ -2,11 +2,11 @@ enyo.depends(
 	"$lib/layout",
 	"$lib/onyx",	// To theme Onyx using Theme.less, change this line to $lib/onyx/source,
 	//"Theme.less",	// uncomment this line, and follow the steps described in Theme.less
-	"App.css",
 	"App.js",
 	"Video.js",
 	"Head.js",
 	"Input.js",
 	"Input.css",
-	"Sequence.js"
+	"Sequence.js",
+	"App.css"
 );
