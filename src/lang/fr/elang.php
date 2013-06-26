f9ee9a51d821772b6cc10ba6c85a2cdae25d213b
Christophe Demko <chdemko@gmail.com>
2013-06-26 23:16:28 +0200

    Add french translation

---
+++ src/lang/fr/elang.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * French strings for elang
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package     mod
+ * @subpackage  elang
+ * @copyright   2013 University of La Rochelle, France
+ * @license     http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html CeCILL-B license
+ *
+ * @since       0.0.3
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['elang:addinstance'] = 'Ajouter une instance';
+$string['elang'] = "Paysage d'écoute";
+$string['elangname_help'] = "Entrez le nom de l'exercice";
+$string['elangname'] = "Titre de l'exercice";
+$string['elang:view'] = 'Vue';
+$string['filearea_poster'] = 'Posters';
+$string['filearea_videos'] = 'Vidéos';
+$string['formatname'] = '%s [%s]';
+$string['language_config'] = 'Sélectionnez la liste des langues possibles pour ce site moodle';
+$string['language'] = 'Langue';
+$string['left_config'] = 'Cela définit la marge de gauche pour les fichiers pdf générés';
+$string['left_error'] = 'Vous devez entrez un nombre. 20 est une valeur typique.';
+$string['left_help'] = 'Cela définit la marge de gauche pour les fichiers pdf générés';
+$string['left'] = 'Marge gauche des fichiers pdf';
+$string['limit_config'] = 'Cela définit la valeur par défaut de la limite des listes.';
+$string['limit_error'] = 'Vous devez entrez un nombre. 10 est une valeur typique.';
+$string['limit_help'] = 'Cela définit la valeur de la limite des listes.';
+$string['limit'] = 'Limite des listes';
+$string['modulename'] = "Paysage d'écoute";
+$string['modulename_help'] = "Utilisez le module «Paysage d'écoute» pour...";
+$string['modulenameplural'] = "Paysages d'écoute";
+$string['noelangs'] = "Aucun module «Paysage d'écoute» dans ce cours";
+$string['pdfcue'] = '%s. %s --> %s';
+$string['pdfsubtitle'] = 'Exercice: %s (%s)';
+$string['pdftitle'] = 'Cours: %s';
+$string['pluginadministration'] = "Administration du module «Paysage d'écoute»";
+$string['pluginname'] = "Paysage d'écoute";
+$string['poster_help'] = 'Déposez une image qui représente la vidéo.';
+$string['postermaxsize_config'] = 'Cela définit la taille maximum des posters en octets';
+$string['postermaxsize'] = 'Taille maximum des posters';
+$string['poster'] = 'Poster';
+$string['repeatedunderscore_config'] = 'Cela définit le nombre de sous-tirets utilisés pour afficher les textes à deviner. i.e. si ce nombre est 10, le texte «devinez ce texte» sera représenté par 20 sous-tirets';
+$string['repeatedunderscore_error'] = 'Vous devez entrez un nombre. 10 est une valeur typique.';
+$string['repeatedunderscore_help'] = 'Cela définit le nombre de sous-tirets utilisés pour afficher les textes à deviner. i.e. si ce nombre est 10, le texte «devinez ce texte» sera représenté par 20 sous-tirets';
+$string['repeatedunderscore'] = 'Nombre de sous-tiret répétés';
+$string['settings'] = 'Réglage spécifique du module';
+$string['showlanguage_config'] = 'Cela affiche la langue dans le titre du module';
+$string['showlanguage_help'] = 'Cela affiche la langue dans le titre du module';
+$string['showlanguage'] = 'Afficher la langue';
+$string['size_config'] = 'Cela définit la taille de la police dans le pdf';
+$string['size_error'] = 'Vous devez entrez un nombre. 16 est une valeur typique.';
+$string['size_help'] = 'Cela définit la taille de la police dans le pdf';
+$string['size'] = 'Taille de la police dans le pdf';
+$string['subtitle_help'] = "Déposez un fichier en utilisant l'encodage UTF-8 au format webvtt ou srt";
+$string['subtitlemaxsize_config'] = 'Cela définit la taille maximum des fichiers de sous-titres en octets';
+$string['subtitlemaxsize'] = 'Taille maximum des fichiers de sous-titres';
+$string['subtitle'] = 'Sous-titres';
+$string['timeout_config'] = "Temps d'attente maximum entre le serveur moodle et le navigateur pour les requêtes";
+$string['timeout'] = "Temps d'attente";
+$string['titlelength_config'] = 'Cela définit la longueur du titre des séquences';
+$string['titlelength'] = 'Longueur du titre des séquence';
+$string['titlelength_error'] = 'Vous devez entrez un nombre. 100 est une valeur typique.';
+$string['titlelength_help'] = 'Cela définit la longueur du titre des séquences';
+$string['top_config'] = 'Cela définit la marge du haut pour les fichiers pdf générés';
+$string['top_error'] = 'Vous devez entrez un nombre. 20 est une valeur typique.';
+$string['top_help'] = 'Cela définit la marge du haut pour les fichiers pdf générés';
+$string['top'] = 'Marge haute des fichiers pdf';
+$string['update_warning'] = "Si vous modifiez un exercice, toute l'activité des étudiants sera effacée pour cet exercice";
+$string['upload'] = "Déposez des fichiers vidéos pour l'exercice (ogv, webm, mp4)";
+$string['videomaxsize_config'] = 'Cela définit la taille maximum des fichiers de vidéos en octets';
+$string['videomaxsize'] = 'Taille maximum des fichiers vidéos';
+$string['videos_help'] = 'Vous pouvez déposez plusieurs vidéos en utilisant les extensions communément admises pour le web (ogv, wemb, mp4).';
+$string['videos'] = 'Vidéos';
+
