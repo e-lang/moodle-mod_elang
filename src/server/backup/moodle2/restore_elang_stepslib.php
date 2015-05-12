e9b03306ae85d1f01c2ca7e3ac14cd839c56496a
Gabriel-AL <ganimograph@gmail.com>
2015-05-12 16:25:34 +0200

    Implement backup/restore

--- src/server/backup/moodle2/restore_elang_stepslib.php
+++ src/server/backup/moodle2/restore_elang_stepslib.php
@@ -12,6 +12,20 @@
  *
  * @since       1.1.0
  */
+
+/**
+ * Steps to restore an elang activity
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package     Mod
+ * @subpackage  elang
+ * @copyright   2013-2015 University of La Rochelle, France
+ * @license     http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html CeCILL-B license
+ *
+ * @since       1.1.0
+ */
 class restore_elang_activity_structure_step extends restore_activity_structure_step
 {
 	/**
